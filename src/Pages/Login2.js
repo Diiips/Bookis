8a67fbfaa3dca22db10c6e5f649f7650694c3342
Diiips <shendagedipali73@gmail.com>
2024-12-03 23:45:13 +0530

    login

--- src/Pages/Login2.js
+++ src/Pages/Login2.js
@@ -12,55 +12,56 @@ const Form = () => {
 
     <div className="LoginForm-page">
 
-    
-    <div className="formbox">
-      <h1 id="title">{isSignUp ? "Sign Up" : "Sign In"}</h1>
-      <form>
+
+      <div className="formbox">
+        <h1 id="title">{isSignUp ? "Sign Up" : "Sign In"}</h1>
+        <form>
 
 
-        <div className="input-group">
-          {isSignUp && (
-            <div className="input-field" id="nameField">
-              <i className="fa-solid fa-user"></i>
-              <input type="text" placeholder="Name" />
+          <div className="input-group">
+            {isSignUp && (
+              <div className="input-field" id="nameField">
+                <i className="fa-solid fa-user"></i>
+                <input type="text" placeholder="Name" />
+              </div>
+            )}
+            <div className="input-field" id="emailfield">
+              <i className="fa-solid fa-envelope"></i>
+              <input type="email" placeholder="Email Id" autocomplete="new-email" />
             </div>
-          )}
-          <div className="input-field" id="emailfield">
-            <i className="fa-solid fa-envelope"></i>
-            <input type="email" placeholder="Email Id"/>
+            <div className="input-field" id="passwordfield">
+              <i className="fa-solid fa-lock"></i>
+              <input type="password" placeholder="Password" autocomplete="new-password" />
+            </div>
+
+            <p>
+              Lost Password? <a href="#">Click Here!</a>
+            </p>
           </div>
-          <div className="input-field" id="passwordfield">
-            <i className="fa-solid fa-lock"></i>
-            <input type="password" placeholder="Password" />
+
+
+          <div className="btn-field">
+            <button
+              type="button"
+              id="signupBtn"
+              className={isSignUp ? "" : "disable"}
+              onClick={() => setIsSignUp(true)}
+            >
+              Sign Up
+            </button>
+            <button
+              type="button"
+              id="signinBtn"
+              className={!isSignUp ? "" : "disable"}
+              onClick={toggleForm}
+            >
+              Sign In
+            </button>
           </div>
-          <p>
-            Lost Password? <a href="#">Click Here!</a>
-          </p>
-        </div>
 
 
-        <div className="btn-field">
-          <button
-            type="button"
-            id="signupBtn"
-            className={isSignUp ? "" : "disable"}
-            onClick={() => setIsSignUp(true)}
-          >
-            Sign Up
-          </button>
-          <button
-            type="button"
-            id="signinBtn"
-            className={!isSignUp ? "" : "disable"}
-            onClick={toggleForm}
-          >
-            Sign In
-          </button>
-        </div>
-
-
-      </form>
-    </div>
+        </form>
+      </div>
     </div>
   );
 };
